ab1757abba821ad2749fa416711c590bed6f2e76
Petr Boháč <pbohac@windowslive.com>
2023-03-24 10:51:26 +0100

    Add project files.

---
+++ vosplzen.sem2h1/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using vosplzen.sem2h1.Data;
+
+namespace vosplzen.sem2h1.Controllers
+{
+
+    [ApiController]
+    [Route("Students")]
+    public class DatetimeController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public DatetimeController(ApplicationDbContext context) {
+
+            _context = context;
+        }
+        
+        [HttpGet]
+        [Route("List")]
+        public IActionResult Getlist()
+        {
+
+            var result = _context.Students.ToList();
+            return new JsonResult(result);
+
+        }
+
+
+
+
+    }
+}
---
+++ vosplzen.sem2h1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using vosplzen.sem2h1.Data.Model;
+
+namespace vosplzen.sem2h1.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
